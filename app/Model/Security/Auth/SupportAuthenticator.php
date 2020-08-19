766662cf8d481edf768e80b176606689f9d4814f
MilanTuryna <miloslavturyna@seznam.cz>
2020-08-19 17:10:50 +0200

    Přidán Authenticator pro Helpdesk

---
+++ app/Model/Security/Auth/SupportAuthenticator.php
@@ -0,0 +1,91 @@
+<?php
+
+
+namespace App\Model\Security\Auth;
+
+
+use App\Model\API\AuthMe;
+use App\Model\API\Plugin\LuckPerms;
+use App\Model\Panel\AuthMeRepository;
+use App\Model\Panel\MojangRepository;
+use App\Model\Security\Exceptions\AuthException;
+use Nette\Http\Session;
+
+/**
+ * Class SupportAuthenticator
+ * @package App\Model\Security\Auth
+ */
+class SupportAuthenticator implements IAuthenticator
+{
+    const SESSION_SECTION = "helpDesk_login";
+
+    private AuthMeRepository $authMeRepository;
+    private Session $session;
+    private LuckPerms $luckPerms;
+    private MojangRepository $mojangRepository;
+
+    /**
+     * SupportAuthenticator constructor.
+     * @param AuthMeRepository $authMeRepository
+     * @param Session $session
+     * @param LuckPerms $luckPerms
+     * @param MojangRepository $mojangRepository
+     */
+    public function __construct(AuthMeRepository $authMeRepository, Session $session, LuckPerms $luckPerms, MojangRepository $mojangRepository)
+    {
+        $this->authMeRepository = $authMeRepository;
+        $this->session = $session;
+        $this->luckPerms = $luckPerms;
+        $this->mojangRepository = $mojangRepository;
+    }
+
+    /**
+     * @inheritDoc
+     * @throws AuthException
+     */
+    public function login(array $credentials, string $expiration = self::EXPIRATION): void
+    {
+
+        [$name, $password] = $credentials;
+        $user = $this->authMeRepository->findByUsername($name);
+        $section = $this->session->getSection(self::SESSION_SECTION);
+        $section->setExpiration($expiration);
+
+        if($user && @AuthMe::isValidLogin($password, $user->password)) {
+            if($this->luckPerms->isUserHelper($this->mojangRepository->getUUID($name))) {
+                $section->id = $user->id;
+            } else {
+                throw new AuthException('K tomuto webu nemáš jako hráč přístup!');
+            }
+        } else {
+            throw new AuthException('Zadal jsi nesprávný nick nebo heslo!');
+        }
+    }
+
+    /**
+     * @throws AuthException
+     */
+    public function logout(): void
+    {
+        $section = $this->session->getSection(self::SESSION_SECTION);
+        if($section->id) {
+            $section->remove();
+        } else {
+            throw new AuthException("Nemůžeš se odhlašovat, když nejsi přihlášený!");
+        }
+    }
+
+    /**
+     * @return AuthMeRepository
+     */
+    public function getAuthMeRepository() {
+        return $this->authMeRepository;
+    }
+
+    /**
+     * @return MojangRepository
+     */
+    public function getMojangRepository() {
+        return $this->mojangRepository;
+    }
+}
