597dab292b7ace747b4d45de075a5584c66c1ffa
MilanTuryna <miloslavturyna@seznam.cz>
2020-09-27 13:49:53 +0200

    Added new methods to Events class

--- app/Model/API/Plugin/Events.php
+++ app/Model/API/Plugin/Events.php
@@ -5,6 +5,7 @@ namespace App\Model\API\Plugin;
 
 
 use Nette\Database\Context;
+use Nette\Database\Table\Selection;
 
 /**
  * Class Events
@@ -20,9 +21,32 @@ class Events
     /**
      * Events constructor.
      * @param Context $context
+     * database.events
      */
     public function __construct(Context $context)
     {
         $this->context = $context;
+    }
+
+    /**
+     * @return Selection
+     */
+    public function findAllEvents() {
+        return $this->context->table(self::EVENTS_TABLE);
+    }
+
+    /**
+     * @return Selection
+     */
+    public function getActivePlayers() {
+        return $this->context->table(self::PLAYERS_TABLE)->where("event_passed > ? OR event_giveup > ?", 1,1);
+    }
+
+    public function getPlayerById($id) {
+        return $this->context->table("event_players")->where("id = ?", $id);
+    }
+
+    public function deleteRecordById($id) {
+        return $this->context->table("event_players")->where("id = ?", $id)->delete();
     }
 }
