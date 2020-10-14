ff558b46476a9de0fee4993f8b0473e69e9caee0
MilanTuryna <miloslavturyna@seznam.cz>
2020-10-14 11:45:51 +0200

    Added some methods to new plugin-class IConomy for economy manipulation

--- app/Model/API/Plugin/IConomy.php
+++ app/Model/API/Plugin/IConomy.php
@@ -4,6 +4,7 @@
 namespace App\Model\API\Plugin;
 
 use Nette\Database\Context;
+use Nette\Database\Table\Selection;
 
 /**
  * Class IConomy
@@ -15,6 +16,8 @@ class IConomy
 {
     private Context $context;
 
+    const TABLE_NAME = "iconomy";
+
     /**
      * IConomy constructor.
      * @param Context $context
@@ -22,5 +25,45 @@ class IConomy
     public function __construct(Context $context)
     {
         $this->context = $context;
+    }
+
+    /**
+     * @param $name
+     * @return Selection
+     */
+    public function getRowByName($name) {
+        return $this->context->table(self::TABLE_NAME)->where("username = ?", $name);
+    }
+
+    /**
+     * @param $id
+     * @return Selection
+     */
+    public function getRowById(int $id) {
+        return $this->context->table(self::TABLE_NAME)->wherePrimary($id);
+    }
+
+    /**
+     * @param $id
+     * @return int
+     */
+    public function deleteRowById(int $id) {
+        return $this->context->table(self::TABLE_NAME)->wherePrimary($id)->delete();
+    }
+
+    /**
+     * @param $name
+     * @return Selection
+     */
+    public function deleteRowByName($name) {
+        return $this->context->table(self::TABLE_NAME)->where("username = ?", $name);
+    }
+
+    /**
+     * @param int $id
+     * @return Selection
+     */
+    public function updateRowById(int $id) {
+        return $this->context->table(self::TABLE_NAME)->wherePrimary($id);
     }
 }
