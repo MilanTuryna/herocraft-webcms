77b33e2f4adcfd4c4cd131521845e48daeaa02ce
MilanTuryna <miloslavturyna@seznam.cz>
2020-10-15 14:41:45 +0200

    Added IDE predefined annotations

--- app/Presenters/AdminModule/MinecraftClassicPresenter.php
+++ app/Presenters/AdminModule/MinecraftClassicPresenter.php
@@ -8,10 +8,19 @@ use App\Model\API\Plugin\Classic\Economy;
 use App\Model\Security\Auth\Authenticator;
 use App\Presenters\AdminBasePresenter;
 
+/**
+ * Class MinecraftClassicPresenter
+ * @package App\Presenters\AdminModule
+ */
 class MinecraftClassicPresenter extends AdminBasePresenter
 {
     private Economy $classicEconomy;
 
+    /**
+     * MinecraftClassicPresenter constructor.
+     * @param Authenticator $authenticator
+     * @param Economy $classicEconomy
+     */
     public function __construct(Authenticator $authenticator, Economy $classicEconomy)
     {
         parent::__construct($authenticator);
--- app/Presenters/AdminModule/MinecraftSeniorPresenter.php
+++ app/Presenters/AdminModule/MinecraftSeniorPresenter.php
@@ -4,9 +4,21 @@
 namespace App\Presenters\AdminModule;
 
 
+use App\Model\Security\Auth\Authenticator;
 use App\Presenters\AdminBasePresenter;
 
+/**
+ * Class MinecraftSeniorPresenter
+ * @package App\Presenters\AdminModule
+ */
 class MinecraftSeniorPresenter extends AdminBasePresenter
 {
-
+    /**
+     * MinecraftSeniorPresenter constructor.
+     * @param Authenticator $authenticator
+     */
+    public function __construct(Authenticator $authenticator)
+    {
+        parent::__construct($authenticator);
+    }
 }
