e0ee8b1d497e6465e46d0d3b9c0f47ca40c3d606
MilanTuryna <miloslavturyna@seznam.cz>
2020-10-15 14:40:43 +0200

    Added new constructor parameter (Classic\Economy $classicEconomy)

--- app/Presenters/AdminModule/MinecraftClassicPresenter.php
+++ app/Presenters/AdminModule/MinecraftClassicPresenter.php
@@ -4,9 +4,18 @@
 namespace App\Presenters\AdminModule;
 
 
+use App\Model\API\Plugin\Classic\Economy;
+use App\Model\Security\Auth\Authenticator;
 use App\Presenters\AdminBasePresenter;
 
 class MinecraftClassicPresenter extends AdminBasePresenter
 {
+    private Economy $classicEconomy;
 
+    public function __construct(Authenticator $authenticator, Economy $classicEconomy)
+    {
+        parent::__construct($authenticator);
+
+        $this->classicEconomy = $classicEconomy;
+    }
 }
