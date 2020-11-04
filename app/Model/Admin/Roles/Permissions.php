1b8a3e344b487f30f1d2cb9b5e5d29758178d330
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-04 21:58:10 +0100

    Added Permissions::getSelectBox() and Permissions::getAllPermissions()

--- app/Model/Admin/Roles/Permissions.php
+++ app/Model/Admin/Roles/Permissions.php
@@ -24,4 +24,36 @@ class Permissions
 
     const ADMIN_GLOBAL_SETTINGS = "admin.global_settings";
     const ADMIN_UPLOAD = "admin.upload";
+
+    /**
+     * Returning a associative array with constant permissions nodes and user-friendly description of this permission (will be used for select/radios inputs)
+     * @return array
+     */
+    public static function getSelectBox() {
+        return [
+            self::ADMIN_ARTICLES => "Správa článků",
+            self::ADMIN_PAGES => "Správa stránek",
+            self::ADMIN_CATEGORIES => "Správa kategorií",
+            self::ADMIN_USERS => "Správa uživatelů/administrátorů",
+
+            self::ADMIN_MC_CHATLOG => "Minecraft > Chatlog",
+            self::ADMIN_MC_BANLIST => "Minecraft > Banlist",
+            self::ADMIN_MC_IPBANLIST => "Minecraft > IP banlist",
+
+            self::ADMIN_MC_GAMES => "Minecraft > Games",
+            self::ADMIN_MC_SENIOR => "Minecraft > Senior",
+            self::ADMIN_MC_CLASSIC => "Minecraft > Classic",
+
+            self::ADMIN_GLOBAL_SETTINGS => "Nastavení webu",
+            self::ADMIN_UPLOAD => "Upload obrázků",
+        ];
+    }
+
+    /**
+     * Returning a array with all permissions nodes, will be used Fulladmin group permissions
+     * @return array
+     */
+    public static function getAllPermissions() {
+        return array_keys(self::getSelectBox());
+    }
 }
