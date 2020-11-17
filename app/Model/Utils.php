af1d0f4e595080ac701c35af6a40a2ab9cb4b654
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-17 13:56:45 +0100

    Added substrWithoutHTML static function in Utils for substring html formatted text without breaking html tags.

--- app/Model/Utils.php
+++ app/Model/Utils.php
@@ -4,6 +4,7 @@
 namespace App\Model;
 
 use App;
+use DOMDocument;
 
 /**
  *
@@ -43,5 +44,30 @@ class Utils
      */
     public static function sqlNow() {
         return date('Y-m-d H:i:s');
+    }
+
+    public static function substrWithoutHTML($value, $limit) {
+        if (mb_strwidth($value, 'UTF-8') <= $limit) {
+            return $value;
+        }
+
+        // Strip text with HTML tags, sum html len tags too.
+        // Is there another way to do it?
+        do {
+            $len          = mb_strwidth($value, 'UTF-8');
+            $len_stripped = mb_strwidth(strip_tags($value), 'UTF-8');
+            $len_tags     = $len - $len_stripped;
+
+            $value = mb_strimwidth($value, 0, $limit + $len_tags, '', 'UTF-8');
+        } while ($len_stripped > $limit);
+
+        // Load as HTML ignoring errors
+        $dom = new DOMDocument();
+        @$dom->loadHTML('<?xml encoding="utf-8" ?>'.$value, LIBXML_HTML_NODEFDTD);
+
+        // Fix the html errors
+        $value = $dom->saveHtml($dom->getElementsByTagName('body')->item(0));
+        $value = mb_strimwidth($value, 6, mb_strwidth($value, 'UTF-8') - 13, '', 'UTF-8'); // <body> and </body>
+        return preg_replace('/<(\w+)\b(?:\s+[\w\-.:]+(?:\s*=\s*(?:"[^"]*"|"[^"]*"|[\w\-.:]+))?)*\s*\/?>\s*<\/\1\s*>/', '', $value);
     }
 }
