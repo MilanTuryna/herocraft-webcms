8cdaba757fbd4084dcf9696e1d52068dea3194bf
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-22 10:23:41 +0100

    Deleted GROUP BY (SQL) from getHelpers method

--- app/Model/API/Plugin/LuckPerms.php
+++ app/Model/API/Plugin/LuckPerms.php
@@ -84,7 +84,7 @@ class LuckPerms
      * @return array|IRow[]
      */
     public function getHelpers() {
-        return $this->context->query("SELECT t2.username, t1.permission, t1.server FROM luckperms_user_permissions AS t1 LEFT JOIN luckperms_players AS t2 ON t1.uuid = t2.uuid WHERE permission = ? OR permission = ? OR permission = ? OR permission = ? OR permission = ?",
+        return $this->context->query("SELECT t2.username, t1.permission, t1.server FROM luckperms_user_permissions AS t1 LEFT JOIN luckperms_players AS t2 ON t1.uuid = t2.uuid WHERE permission = ? OR permission = ? OR permission = ? OR permission = ? OR permission = ? ORDER BY t2.username DESC",
             self::GROUPS['helper'], self::GROUPS['owner'], self::GROUPS['admin'], self::GROUPS['helpdesk'], self::GROUPS['implement-web'])->fetchAll();
 
     }
