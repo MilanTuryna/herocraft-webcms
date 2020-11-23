4d5c7775dc0937294b7a12a9e59b1cca1630f7c5
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-23 23:31:00 +0100

    Added annotations to UserRepository

--- app/Model/UserRepository.php
+++ app/Model/UserRepository.php
@@ -5,28 +5,56 @@ namespace App\Model;
 
 
 use Nette\Database\Context;
+use Nette\Database\IRow;
+use Nette\Database\Table\ActiveRow;
 
+/**
+ * Class UserRepository
+ * @package App\Model
+ */
 class UserRepository
 {
     private Context $context;
 
+    /**
+     * UserRepository constructor.
+     * @param Context $context
+     */
     public function __construct(Context $context)
     {
         $this->context = $context;
     }
 
+    /**
+     * @return array
+     */
     public function findAll(): array {
         return $this->context->table('admin')->fetchAll();
     }
 
+    /**
+     * @param $name
+     * @return IRow|ActiveRow|null
+     */
     public function findByName($name) { // or email
         return $this->context->table('admin')->where('name = ? OR email = ?', $name, $name)->fetch();
     }
 
+    /**
+     * @param $id
+     * @return ActiveRow|null
+     */
     public function findById($id) {
         return $this->context->table('admin')->get($id);
     }
 
+    /**
+     * @param $user
+     * @param $email
+     * @param $pass
+     * @param $permissions
+     * @return bool|int|ActiveRow
+     */
     public function addUser($user, $email, $pass, $permissions) {
         return $this->context->table('admin')->insert([
             'name' => $user,
