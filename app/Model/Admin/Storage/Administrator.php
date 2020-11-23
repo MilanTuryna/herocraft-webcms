ea176de392b21c7d4c0e17b764104c7d6aa9b6ee
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-23 22:39:09 +0100

    Created class Administrator that represent administration user.

---
+++ app/Model/Admin/Storage/Administrator.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Model\Admin\Storage;
+
+/**
+ * Class Administrator
+ *
+ * Class represent user in administration
+ */
+class Administrator
+{
+    private string $name;
+    private string $email;
+    private int $id;
+    private array $permissions;
+
+    public function __construct(string $name, string $email, int $id, array $permissions)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->id = $id;
+        $this->permissions = $permissions;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPermissions(): array
+    {
+        return $this->permissions;
+    }
+}
