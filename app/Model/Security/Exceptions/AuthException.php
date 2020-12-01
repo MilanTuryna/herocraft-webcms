84569531425b7fa48b9a2529c129ec9084bf3ea5
MilanTuryna <miloslavturyna@seznam.cz>
2020-12-01 21:30:49 +0100

    Added description of class in Security\Exceptions\AuthException

--- app/Model/Security/Exceptions/AuthException.php
+++ app/Model/Security/Exceptions/AuthException.php
@@ -3,5 +3,10 @@
 
 namespace App\Model\Security\Exceptions;
 
-
+/**
+ * Class AuthException
+ * @package App\Model\Security\Exceptions
+ *
+ * Exception class for catching throws about auth errors (bad nick or password, no permissions to authorize..)
+ */
 final class AuthException extends \Exception {};
