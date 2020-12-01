fd7f407f8caa6e6a69126d587110d7a38b4eb472
MilanTuryna <miloslavturyna@seznam.cz>
2020-12-01 08:02:25 +0100

    Added @param LinkGenerator $linkGenerator to Callbacks\Discord constructor for generating URL by parameter $routes in Discord::notify (for adding URLs to discord embed message) and implemented it in DI configuration (common.neon)

--- app/Model/DI/Tickets/Callbacks/Discord.php
+++ app/Model/DI/Tickets/Callbacks/Discord.php
@@ -3,6 +3,8 @@
 namespace App\Model\DI\Tickets\Callbacks;
 
 use App\Model\Panel\Object\Ticket;
+use Nette\Application\LinkGenerator;
+use Nette\Application\UI\InvalidLinkException;
 use Nette\SmartObject;
 use Nette\Utils\Json;
 use Nette\Utils\JsonException;
@@ -21,6 +23,7 @@ class Discord
     private string $color;
     private string $username;
     private ?string $logo;
+    private LinkGenerator $linkGenerator;
 
     /**
      * Discord constructor.
@@ -29,62 +32,63 @@ class Discord
      * @param string $color
      * @param string|null $username
      * @param string|null $logo
+     * @param LinkGenerator $linkGenerator
      */
-    public function __construct(bool $enabled, ?string $url, ?string $color, ?string $username, ?string $logo)
+    public function __construct(bool $enabled, ?string $url, ?string $color, ?string $username, ?string $logo, LinkGenerator $linkGenerator)
     {
         $this->enabled = $enabled;
         $this->url = $url ?: '';
         $this->color = $color ?: self::DEFAULT_EMBED_COLOR;
         $this->username = $username ?: "Tickety";
         $this->logo = $logo;
+        $this->linkGenerator = $linkGenerator;
     }
 
     /**
      * @param Ticket $ticket
+     * @param array $routes
      * @return bool
+     * @throws InvalidLinkException
+     * @throws JsonException
      */
-    public function notify(Ticket $ticket): bool {
+    public function notify(Ticket $ticket, array $routes): bool {
         if(!$this->isEnabled()) return false;
-        try {
-            $response = Json::encode([
-                "content" => null,
-                "username" => $this->username,
-                "avatar_url" => $this->logo,
-                "tts" => false,
-                "embeds" => [[
-                    "title" => "Nový ticket č. " . $ticket->getId(),
-                    "description" => "Hráč ".$ticket->getAuthor()." právě vytvořil nový ticket, nezapomeňte ho zkontrolovat!",
-                    "timestamp" => date("c", strtotime("now")),
-                    "color" => $this->color,
-                    "fields" => [
-                        [
-                            "name" => "Informace o ticketu",
-                            "value" =>
-                                "Autor: **[".$ticket->getAuthor()."](https://petrzelkaasd.czewd)**\n" .
-                                "Název: **".substr($ticket->getName(), 0, 30)."**\n" .
-                                "ID ticketu: **#" . $ticket->getId() . "**\n" .
-                                "Zvolený předmět: **".$ticket->getSubject()."**\n" .
-                                "Zobrazit ticket: **[ZDE](https://petrzelkads.czads)**"
-                        ],
+        [$statsRoute, $helpDeskRoute] = $routes;
+        $response = Json::encode([
+            "content" => null,
+            "username" => $this->username,
+            "avatar_url" => $this->logo,
+            "tts" => false,
+            "embeds" => [[
+                "title" => "Nový ticket č. " . $ticket->getId(),
+                "description" => "Hráč ".$ticket->getAuthor()." právě vytvořil nový ticket, nezapomeňte ho zkontrolovat!",
+                "timestamp" => date("c", strtotime("now")),
+                "color" => $this->color,
+                "fields" => [
+                    [
+                        "name" => "Informace o ticketu",
+                        "value" =>
+                            "Autor: **[".$ticket->getAuthor()."](". $this->linkGenerator->link($statsRoute, [$ticket->getAuthor()]) .")**\n" .
+                            "Název: **".substr($ticket->getName(), 0, 30)."**\n" .
+                            "ID ticketu: **#" . $ticket->getId() . "**\n" .
+                            "Zvolený předmět: **".$ticket->getSubject()."**\n" .
+                            "Zobrazit ticket: **[ZDE](".$this->linkGenerator->link($helpDeskRoute, [$ticket->getId()]).")**"
                     ],
-                  "footer" => [
-                      "text" => "Notifikace z webového systému"
-                  ]
-                ]]
-            ]);
-            $ch = curl_init($this->url);
-            curl_setopt( $ch, CURLOPT_HTTPHEADER, ['Content-type: application/json']);
-            curl_setopt( $ch, CURLOPT_POST, 1);
-            curl_setopt( $ch, CURLOPT_POSTFIELDS, $response);
-            curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-            curl_setopt( $ch, CURLOPT_HEADER, 0);
-            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-            $curlexec = curl_exec($ch);
-            curl_close($ch);
-        } catch (JsonException $exception) {
-            return false;
-        }
-
+                ],
+                "footer" => [
+                    "text" => "Notifikace z webového systému"
+                ]
+            ]]
+        ]);
+        $ch = curl_init($this->url);
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, ['Content-type: application/json']);
+        curl_setopt( $ch, CURLOPT_POST, 1);
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, $response);
+        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt( $ch, CURLOPT_HEADER, 0);
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+        $curlexec = curl_exec($ch);
+        curl_close($ch);
         return true;
     }
 
