d569c06dde0832781a9a5342492f465f9a1d6cd1
MilanTuryna <miloslavturyna@seznam.cz>
2020-12-05 10:55:02 +0100

    Added hidden non required input "returnRoute" for returning specific route after success login in SignInForm.php

--- app/Forms/Panel/SignInForm.php
+++ app/Forms/Panel/SignInForm.php
@@ -7,24 +7,37 @@ use App\Model\Security\Auth\PluginAuthenticator;
 
 use Nette\Application\AbortException;
 use Nette\Application\UI\Form;
+use Nette\Application\UI\InvalidLinkException;
 use Nette\Application\UI\Presenter;
 
+use Nette\Routing\Route;
 use stdClass;
 
 class SignInForm
 {
     private PluginAuthenticator $pluginAuthenticator;
     private Presenter $presenter;
+    private ?string $returnRoute;
 
     /**
      * SignInForm constructor.
      * @param PluginAuthenticator $pluginAuthenticator
      * @param Presenter $presenter
+     * @param string|null $returnRoute
      */
-    public function __construct(PluginAuthenticator $pluginAuthenticator, Presenter $presenter)
+    public function __construct(PluginAuthenticator $pluginAuthenticator, Presenter $presenter, ?string $returnRoute = null)
     {
         $this->pluginAuthenticator = $pluginAuthenticator;
         $this->presenter = $presenter;
+        $this->returnRoute = $returnRoute;
+    }
+
+    /**
+     * @param string|null $returnRoute
+     */
+    public function setReturnRoute(?string $returnRoute): void
+    {
+        $this->returnRoute = $returnRoute;
     }
 
     public function create(): Form {
@@ -33,6 +46,9 @@ class SignInForm
             ->setRequired();
         $form->addPassword('password', 'Heslo')->setRequired();
         $form->addSubmit('submit')->setRequired();
+        $form->addHidden('returnRoute')
+            ->setDefaultValue($this->returnRoute)
+            ->setRequired(false);
         $form->onSuccess[] = [$this, 'success'];
         $form->onError[] = function() use ($form) {
             foreach($form->getErrors() as $error) $this->presenter->flashMessage($error, 'error');
@@ -49,6 +65,10 @@ class SignInForm
         try {
             $this->pluginAuthenticator->login([$values->name, $values->password]);
             $this->presenter->flashMessage('Byl jsi úspěšně autorizován a přihlášen!', 'success');
+            if($values->returnRoute) try {
+                    $this->presenter->link($values->returnRoute);
+                    $this->presenter->redirect($values->returnRoute);
+                } catch (InvalidLinkException $invalidLinkException) {}
             $this->presenter->redirect(':Panel:Main:home');
         } catch (AuthException $e) {
             $form->addError($e->getMessage());
