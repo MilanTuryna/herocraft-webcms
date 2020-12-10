e2da10112419b10e5c32aee63add2b39e7ec2a49
MilanTuryna <miloslavturyna@seznam.cz>
2020-12-10 20:17:43 +0100

    Added new parameter Cron $cron to CronPresenter constructor and changed authentication in actionSavingPlaytime($authnetication) (from checking client ip to checking $authentication with password in database)

--- app/Presenters/CronPresenter.php
+++ app/Presenters/CronPresenter.php
@@ -5,6 +5,7 @@ namespace App\Presenters;
 
 
 use App\Model\API\Plugin\LuckPerms;
+use App\Model\DI\Cron;
 use Nette\Application\AbortException;
 use Nette\Application\UI\Presenter;
 use Nette\Database\Context;
@@ -16,23 +17,27 @@ use Nette\Database\Context;
 class CronPresenter extends Presenter
 {
     private Context $context;
+    private Cron $cron;
 
     /**
      * CronPresenter constructor.
      * @param Context $context
+     * @param Cron $cron
      */
-    public function __construct(Context $context)
+    public function __construct(Context $context, Cron $cron)
     {
         parent::__construct();
 
         $this->context = $context;
+        $this->cron = $cron;
     }
 
     /**
+     * @param $authentication
      * @throws AbortException
      */
-    public function actionSavingPlaytime() {
-        if($_SERVER['SERVER_ADDR'] === $_SERVER['REMOTE_ADDR']) {
+    public function actionSavingPlaytime($authentication) {
+        if($authentication === $this->cron->getAuthenticationPassword()) {
             $arr = [];
             $helpers = $this->context->query("SELECT t2.username, t1.uuid, t1.permission, t1.server, t3.playtime FROM luckperms_user_permissions AS t1 LEFT JOIN luckperms_players AS t2 ON t1.uuid = t2.uuid INNER JOIN playtime AS t3 ON t2.username = t3.username WHERE t1.permission = ? OR t1.permission = ? OR t1.permission = ? OR t1.permission=? OR t1.permission=?",
                 LuckPerms::GROUPS['helper'],
