ace1ddf3d6c61b07e1ac07fc4f32358b25097a8f
MilanTuryna <miloslavturyna@seznam.cz>
2020-12-10 20:43:04 +0100

    Changed loop in actionSavingPlaytime (CronPresenter)

--- app/Presenters/CronPresenter.php
+++ app/Presenters/CronPresenter.php
@@ -39,13 +39,16 @@ class CronPresenter extends Presenter
     public function actionSavingPlaytime($authentication) {
         if($authentication === $this->cron->getAuthenticationPassword()) {
             $arr = [];
-            $helpers = $this->context->query("SELECT t2.username, t1.uuid, t1.permission, t1.server, t3.playtime FROM luckperms_user_permissions AS t1 LEFT JOIN luckperms_players AS t2 ON t1.uuid = t2.uuid INNER JOIN playtime AS t3 ON t2.username = t3.username WHERE t1.permission = ? OR t1.permission = ? OR t1.permission = ? OR t1.permission=? OR t1.permission=?",
+            $helpers = $this->context->query("SELECT t2.username, t1.uuid, t1.permission, t1.server, t3.playtime FROM luckperms_user_permissions AS t1 LEFT JOIN luckperms_players AS t2 ON t1.uuid = t2.uuid INNER JOIN playtime AS t3 ON t2.username = t3.username WHERE t1.permission = ? OR t1.permission = ? OR t1.permission = ? OR t1.permission=? OR t1.permission=? GROUP BY username",
                 LuckPerms::GROUPS['helper'],
                 LuckPerms::GROUPS['helpdesk'],
                 LuckPerms::GROUPS['implement-web'],
                 LuckPerms::GROUPS['admin'],
                 LuckPerms::GROUPS['owner'])->fetchAll();
-            foreach ($helpers as $helper) array_push($arr, [$helper->username, $helper->playtime]);
+            foreach ($helpers as $helper) array_push($arr, [
+                "username" => $helper->username,
+                "playtime" => $helper->playtime
+            ]);
             $this->context->table("playtime_week")->insert($arr);
         } {
             $this->flashMessage("Přístup zamítnut.", "danger");
