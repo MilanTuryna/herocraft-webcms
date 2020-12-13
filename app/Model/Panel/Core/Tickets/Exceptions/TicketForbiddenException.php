73a53cb95bb9d203ae14216659046c4ab2fb417c
MilanTuryna <miloslavturyna@seznam.cz>
2020-12-13 12:01:37 +0100

    Created ...\Core\Ticket\Exceptions namespace and TicketForbiddenException & TicketNotFoundException

---
+++ app/Model/Panel/Core/Tickets/Exceptions/TicketForbiddenException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Panel\Core\Tickets\Exceptions;
+
+/**
+ * Class TicketForbiddenException
+ * @package App\Model\Panel\Core\Tickets\Exceptions
+ * Exception for example: The client goes to the ticket that is not his
+ */
+class TicketForbiddenException extends \Exception{}
---
+++ app/Model/Panel/Core/Tickets/Exceptions/TicketNotFoundException.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Model\Panel\Core\Tickets\Exceptions;
+
+/**
+ * Class TicketNotFoundException
+ * @package App\Model\Panel\Core\Tickets\Exceptions
+ */
+class TicketNotFoundException extends \Exception{}
