de23d9230b25bb9c19dd6cd44418d4ba3f491bf3
MilanTuryna <miloslavturyna@seznam.cz>
2020-12-24 14:16:41 +0100

    Created new class Front\Section(string $title, Text $text, string $bgColor)

---
+++ app/Model/Front/Section.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace App\Model\Front;
+
+use App\Model\Front\UI\Button;
+use App\Model\Front\UI\Image;
+use App\Model\Front\UI\Text;
+use Nette\SmartObject;
+
+/**
+ * Class Section
+ * @package App\Model\Front\Object
+ */
+class Section
+{
+    use SmartObject;
+
+    const DEFAULT_BACKGROUND_COLOR = "#ffffff";
+
+    private string $title;
+    private string $bgColor = self::DEFAULT_BACKGROUND_COLOR;
+    private string $imageAlign = "left";
+
+    public Text $text;
+    public ?Button $button;
+    public ?Image $image;
+
+    /**
+     * Section constructor.
+     * @param string $title
+     * @param Text $text
+     * @param string $bgColor
+     */
+    public function __construct(string $title, Text $text, string $bgColor)
+    {
+        $this->title = $title;
+        $this->text = $text;
+        $this->bgColor = $bgColor;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return Text
+     */
+    public function getText(): Text
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $imageAlign
+     */
+    public function setImageAlign(string $imageAlign): void
+    {
+        $this->imageAlign = $imageAlign;
+    }
+}
