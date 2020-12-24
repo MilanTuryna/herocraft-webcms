f0e1314bed2f1a4572f13a6573ce6407a5a5121c
MilanTuryna <miloslavturyna@seznam.cz>
2020-12-24 14:14:16 +0100

    Created interface Front\UI\IElement with method toJSON(): string and __toString()

---
+++ app/Model/Front/UI/IElement.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Model\Front\UI;
+
+/**
+ * Interface IElement
+ */
+interface IElement
+{
+    /**
+     * @return string
+     */
+    public function toJSON(): string;
+
+    /**
+     * @return string
+     */
+    public function __toString(): string;
+}
