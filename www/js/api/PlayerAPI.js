48855c9b4862c7b0b8e1649ab31711c08ba39c19
MilanTuryna <miloslavturyna@seznam.cz>
2020-12-24 07:36:23 +0100

    Moved PlayerAPI.js from js\stats to js\api and changed file path (for importing script) in template Stats\Main\app.latte

--- www/js/api/PlayerAPI.js
+++ www/js/api/PlayerAPI.js
@@ -1,9 +1,17 @@
 class PlayerAPI {
+    /**
+     * @param nickname
+     */
     constructor(nickname) {
         this.apiURL = window.location.origin + "/statistiky/api" + nickname;
         this.nickname = nickname;
     }
 
+    /**
+     * @param then
+     * @param err
+     * @param loaded
+     */
     static getStatus(then, err, loaded) {
         axios
             .get(`${APIPath}/statistiky/api/testtesttesttesttest`)
@@ -12,6 +20,11 @@ class PlayerAPI {
             .finally(loaded)
     }
 
+    /**
+     * @param then
+     * @param err
+     * @param loaded
+     */
     callAPI(then, err, loaded) {
         axios
             .get(this.apiURL)
