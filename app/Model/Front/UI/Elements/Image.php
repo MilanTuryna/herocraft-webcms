2bb2d88263fcfbc7e134062ad90bb0e1cbfef69c
MilanTuryna <miloslavturyna@seznam.cz>
2020-12-24 14:15:34 +0100

    Created new UI\Elements\Image

---
+++ app/Model/Front/UI/Elements/Image.php
@@ -0,0 +1,86 @@
+<?php
+
+
+namespace App\Model\Front\UI;
+
+/**
+ * Class Image
+ * @package App\Model\Front\UI
+ */
+class Image implements IElement
+{
+    private string $url;
+    private string $width = "100%";
+    private string $height = "auto";
+    private string $title;
+    private ?string $alt;
+
+    /**
+     * Image constructor.
+     * @param string $title
+     * @param string $url
+     * @param string $alt
+     */
+    public function __construct(string $title, string $url, string $alt)
+    {
+        $this->title = $title;
+        $this->url = $url;
+        $this->alt = $alt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getWidth(): string
+    {
+        return $this->width;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHeight(): string
+    {
+        return $this->height;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAlt(): ?string
+    {
+        return $this->alt;
+    }
+
+    /**
+     * @return string
+     */
+    public function toJSON(): string
+    {
+        return json_encode(get_object_vars($this)) ?: "{}";
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->toJSON();
+    }
+}
