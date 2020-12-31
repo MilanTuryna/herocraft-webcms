08ae41ecfab54f344ec77de8028fbc27313d1cd4
MilanTuryna <miloslavturyna@seznam.cz>
2020-12-31 13:24:17 +0100

    Creating instance of form component ChangeHeaderSectionForm in createComponent function in AdminModule\ContentPresenter and used this instance in Admin\Content\overview.latte template (form-render)

--- app/Presenters/AdminModule/ContentPresenter.php
+++ app/Presenters/AdminModule/ContentPresenter.php
@@ -4,9 +4,13 @@
 namespace App\Presenters\AdminModule;
 
 
+use App\Forms\Admin\Content\Homepage\ChangeHeaderSectionForm;
+use App\Front\Section\SectionManager;
 use App\Model\Admin\Roles\Permissions;
 use App\Model\Security\Auth\Authenticator;
+use App\Model\SettingsRepository;
 use App\Presenters\AdminBasePresenter;
+use Nette\Application\UI\Form;
 use Nette\SmartObject;
 
 /**
@@ -17,15 +21,30 @@ class ContentPresenter extends AdminBasePresenter
 {
     use SmartObject;
 
+    private SettingsRepository $settingsRepository;
+    private SectionManager $sectionManager;
+
     /**
      * ContentPresenter constructor.
      * @param Authenticator $authenticator
+     * @param SettingsRepository $settingsRepository
+     * @param SectionManager $sectionManager
      * @param string $permissionNode
      */
-    public function __construct(Authenticator $authenticator, string $permissionNode = Permissions::ADMIN_CONTENT_MANAGER)
+    public function __construct(Authenticator $authenticator,
+                                SettingsRepository $settingsRepository,
+                                SectionManager $sectionManager,
+                                string $permissionNode = Permissions::ADMIN_CONTENT_MANAGER)
     {
         parent::__construct($authenticator, $permissionNode);
+
+        $this->settingsRepository = $settingsRepository;
     }
 
-    public function renderOverview() {/* TODO */}
+    /**
+     * @return Form
+     */
+    public function createComponentChangeHeaderSectionForm(): Form {
+        return (new ChangeHeaderSectionForm($this, $this->settingsRepository, $this->sectionManager))->create();
+    }
 }
