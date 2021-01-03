1ef5942ceb6fee25b068d94f1fa8825266b9e0a9
MilanTuryna <miloslavturyna@seznam.cz>
2021-01-03 09:38:37 +0100

    Changed Front\UI\Parts\Section constructor and properties

--- app/Model/Front/UI/Parts/Section.php
+++ app/Model/Front/UI/Parts/Section.php
@@ -17,10 +17,14 @@ class Section
     use SmartObject;
 
     const DEFAULT_BACKGROUND_COLOR = "#ffffff";
+    const DEFAULT_IMAGE_ALIGN = "left";
 
-    private string $title;
-    private string $bgColor = self::DEFAULT_BACKGROUND_COLOR;
-    private string $imageAlign = "left";
+    public string $title;
+    public string $bgColor = self::DEFAULT_BACKGROUND_COLOR;
+    public string $imageAlign;
+    public int $section_view;
+
+    public ?string $anchor;
 
     public Text $text;
     public ?Button $button;
@@ -31,35 +35,22 @@ class Section
      * @param string $title
      * @param Text $text
      * @param string $bgColor
+     * @param string $imageAlign
+     * @param int $section_view
+     * @param string|null $anchor
      */
-    public function __construct(string $title, Text $text, string $bgColor)
+    public function __construct(string $title,
+                                Text $text,
+                                string $bgColor,
+                                int $section_view,
+                                ?string $anchor,
+                                string $imageAlign = Section::DEFAULT_IMAGE_ALIGN)
     {
         $this->title = $title;
         $this->text = $text;
         $this->bgColor = $bgColor;
-    }
-
-    /**
-     * @return string
-     */
-    public function getTitle(): string
-    {
-        return $this->title;
-    }
-
-    /**
-     * @return Text
-     */
-    public function getText(): Text
-    {
-        return $this->text;
-    }
-
-    /**
-     * @param string $imageAlign
-     */
-    public function setImageAlign(string $imageAlign): void
-    {
         $this->imageAlign = $imageAlign;
+        $this->section_view = $section_view;
+        $this->anchor = $anchor;
     }
 }
