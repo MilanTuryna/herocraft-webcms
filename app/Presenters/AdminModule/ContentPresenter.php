70a79894991c83aa38b0f9ef011cc87728940256
MilanTuryna <miloslavturyna@seznam.cz>
2021-01-08 21:26:15 +0100

    Created EditSectionForm instance in createComponent... method and created render&action method for new ContentPresenter routes (AdminModule\ContentPresenter)

--- app/Presenters/AdminModule/ContentPresenter.php
+++ app/Presenters/AdminModule/ContentPresenter.php
@@ -6,12 +6,15 @@ namespace App\Presenters\AdminModule;
 
 use App\Forms\Admin\Content\Homepage\ChangeHeaderSectionForm;
 use App\Forms\Content\Sections\CreateSectionForm;
+use App\Forms\Content\Sections\EditSectionForm;
 use App\Front\SectionRepository;
 use App\Model\Admin\Roles\Permissions;
 use App\Model\Security\Auth\Authenticator;
 use App\Model\SettingsRepository;
 use App\Presenters\AdminBasePresenter;
+use Nette\Application\AbortException;
 use Nette\Application\UI\Form;
+use Nette\Application\UI\Multiplier;
 use Nette\SmartObject;
 
 /**
@@ -43,11 +46,58 @@ final class ContentPresenter extends AdminBasePresenter
         $this->sectionRepository = $sectionRepository;
     }
 
+    public function renderOverview() {
+        $sectionList = [];
+        foreach ($this->sectionRepository->getAllSections() as $activeRow) array_push($sectionList, $this->sectionRepository::parseSection($activeRow));
+        $this->template->sectionList = $sectionList;
+    }
+
+    /**
+     * @param int $id
+     * @param string|null $sectionName
+     * @throws AbortException
+     */
+    public function actionDeleteSection(int $id, ?string $sectionName) {
+        if($this->sectionRepository->deleteSection($id)) {
+            $this->flashMessage("Sekce ".$sectionName?$sectionName.' ':' '."byla úspěšně odstraněna!", "success");
+        } else {
+            $this->flashMessage("Tato sekce nemohla být odstraněna, jelikož neexistuje!", "danger");
+        }
+        $this->redirect("Content:overview");
+    }
+
+    /**
+     * @param int $id
+     * @throws AbortException
+     */
+    public function renderEditSection(int $id) {
+        $section = $this->sectionRepository->getSectionById($id);
+        if($section) {
+            $parsedSection = $this->sectionRepository::parseSection($section);
+            if($parsedSection) {
+                $this->template->section = $parsedSection;
+            } else {
+                $this->flashMessage("Nastala neznámá chyba při zpracovávání sekce z databáze (nikoliv SQL error!)", "danger");
+                $this->redirect("Content:overview");
+            }
+        } else {
+            $this->flashMessage("Nemůžeš editovat sekci, která neexistuje!");
+            $this->redirect("Content:overview");
+        }
+    }
+
     /**
      * @return Form
      */
     public function createComponentChangeHeaderSectionForm(): Form {
         return (new ChangeHeaderSectionForm($this, $this->settingsRepository))->create();
+    }
+
+    /**
+     * @return Multiplier
+     */
+    public function createComponentEditSectionForm(): Multiplier {
+        return new Multiplier(fn(int $sectionId): Form => (new EditSectionForm($this, $this->sectionRepository, $sectionId, "this"))->create());
     }
 
     /**
