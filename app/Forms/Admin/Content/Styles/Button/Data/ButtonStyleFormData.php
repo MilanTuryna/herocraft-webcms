180c0951da6f7d0afd960af263fa34057d830777
MilanTuryna <miloslavturyna@seznam.cz>
2021-01-12 09:12:55 +0100

    Created simple Content\Styles\Button\Data\ButtonStyleFormData class, that representing form for manipulating with button styles (edit & create)

---
+++ app/Forms/Admin/Content/Styles/Button/Data/ButtonStyleFormData.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Forms\Content\Styles\Button\Data;
+
+/**
+ * Class ButtonStyleFormData
+ * @package App\Forms\Content\Styles\Button\Data
+ */
+class ButtonStyleFormData
+{
+    public ?string $name;
+    public ?string $class;
+    public ?string $css;
+}
