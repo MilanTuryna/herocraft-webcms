da2a7706118298db29a64ee325137b484304ac30
MilanTuryna <miloslavturyna@seznam.cz>
2021-01-12 08:40:42 +0100

    Created Model\Front\Styles\ButtonStyles class for getting dynamic button styles from database and implemented it in DI configuration - config\common.neon

---
+++ app/Model/Front/Styles/ButtonStyles.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Front\Styles;
+
+use Nette\Database\Context;
+use Nette\Database\Table\ActiveRow;
+use Nette\Database\Table\IRow;
+use Nette\Http\Response;
+use Nette\SmartObject;
+
+/**
+ * Class ButtonStyles
+ * @package App\Front\Styles
+ */
+class ButtonStyles
+{
+    use SmartObject;
+
+    const DB_TABLE = "button_styles";
+
+    private Context $context;
+    private Response $response;
+
+    /**
+     * ButtonStyles constructor.
+     * @param Context $context
+     */
+    public function __construct(Context $context)
+    {
+        $this->context = $context;
+    }
+
+    /**
+     * @param array $styles
+     * @return array
+     */
+    public static function getSelectBox(array $styles) {
+        $arr = [];
+        foreach ($styles as $style) $arr[$style->class] = $style->name;
+        return $arr;
+    }
+
+    /**
+     * @param $class
+     * @return string
+     */
+    public static function parseClass($class): string {
+        return str_starts_with($class, ".") ? substr($class, 1) : $class;
+    }
+
+    /**
+     * @param $name
+     * @param $class
+     * @param $css
+     * @return iterable
+     */
+    public static function getIterableRow($name, $class, $css): iterable {
+        return [
+            "name" => $name,
+            "class" => self::parseClass($class),
+            "css" => $css
+        ];
+    }
+
+    /**
+     * @return array|IRow[]
+     */
+    public function getStyles(): array {
+        return $this->context->table(self::DB_TABLE)->fetchAll();
+    }
+
+    /**
+     * @param iterable $data
+     * @return bool|int|ActiveRow
+     */
+    public function createStyle(iterable $data) {
+        return $this->context->table(self::DB_TABLE)->insert($data);
+    }
+
+    /**
+     * @param iterable $data
+     * @return int
+     */
+    public function editStyle(iterable $data) {
+        return $this->context->table(self::DB_TABLE)->update($data);
+    }
+
+    /**
+     * @param int $id
+     * @return int
+     */
+    public function deleteStyle(int $id): int {
+        return $this->context->table(self::DB_TABLE)->where("id = ?", $id)->delete();
+    }
+
+    /**
+     * @return Context
+     */
+    public function getContext(): Context
+    {
+        return $this->context;
+    }
+}
