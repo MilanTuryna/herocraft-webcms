b9fa8235b4dd178ad066e8d145b7ec537cfb9fe9
MilanTuryna <miloslavturyna@seznam.cz>
2021-01-12 08:39:20 +0100

    Created Model\Responses\CSSResponse class for sending response with text/css header.

---
+++ app/Model/Responses/CSSResponse.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Model\Responses;
+
+use Nette;
+
+/**
+ * Class CSSResponse
+ * @package App\Model\Responses
+ */
+class CSSResponse implements \Nette\Application\IResponse
+{
+    use Nette\SmartObject;
+
+    private string $content;
+    private string $type;
+    private string $charset;
+
+    /**
+     * CSSResponse constructor.
+     * @param string $content
+     * @param string $charset
+     * @param string $type
+     */
+    public function __construct(string $content, string $charset = 'utf-8', string $type = 'text/css')
+    {
+        $this->content = $content;
+        $this->charset = $charset;
+        $this->type = $type;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function send(Nette\Http\IRequest $httpRequest, Nette\Http\IResponse $httpResponse): void
+    {
+        $httpResponse->setContentType($this->type, $this->charset);
+        echo trim($this->content);
+    }
+}
