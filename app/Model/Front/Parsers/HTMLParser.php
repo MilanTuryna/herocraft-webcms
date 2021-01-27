827273527419158adf6e79fcdce7490a5e6c35a3
MilanTuryna <miloslavturyna@seznam.cz>
2021-01-27 16:54:21 +0100

    Created Parsers\HTMLParser::replaceDoubleDash() that delete string between '--'

--- app/Model/Front/Parsers/HTMLParser.php
+++ app/Model/Front/Parsers/HTMLParser.php
@@ -9,11 +9,22 @@ namespace App\Model\Front\Parsers;
  */
 class HTMLParser
 {
+    const asteriskPattern = "/\*{1,2}(.*?)\*{1,2}/";
+    const doubleDashPattern = "/--(.*?)--/g";
+
     /**
      * @param string $input
      * @return string
      */
     public static function replaceAsterisk(string $input): string {
-        return preg_replace("/\*{1,2}(.*?)\*{1,2}/", "<strong>$1</strong>", $input);
+        return preg_replace(self::asteriskPattern, "<strong>$1</strong>", $input);
+    }
+
+    /**
+     * @param string $input
+     * @return string
+     */
+    public static function replaceDoubleDash(string $input) {
+        return preg_replace(self::doubleDashPattern, "", $input);
     }
 }
