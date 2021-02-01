2bef2154afc7b3a53ea26787c100cc98b8a3cd8d
MilanTuryna <miloslavturyna@seznam.cz>
2021-02-01 16:59:09 +0100

    Created Responses\JSResponse

---
+++ app/Model/Responses/JSResponse.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Model\Responses;
+
+
+use Nette;
+use Nette\Application\IResponse;
+
+/**
+ * Class JSResponse
+ * @package App\Model\Responses
+ */
+class JSResponse implements IResponse
+{
+    use Nette\SmartObject;
+
+    private string $content;
+    private string $type;
+    private string $charset;
+
+    /**
+     * CSSResponse constructor.
+     * @param string $content
+     * @param string $charset
+     * @param string $type
+     */
+    public function __construct(string $content, string $charset = 'utf-8', string $type = 'text/javascript')
+    {
+        $this->content = $content;
+        $this->charset = $charset;
+        $this->type = $type;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function send(Nette\Http\IRequest $httpRequest, Nette\Http\IResponse $httpResponse): void
+    {
+        $httpResponse->setContentType($this->type, $this->charset);
+        echo trim($this->content);
+    }
+}
