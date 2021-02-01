0a866b4ff57c96d9698e33447769d14bfc258b60
MilanTuryna <miloslavturyna@seznam.cz>
2021-02-01 22:17:26 +0100

    Created abstract presenter (WebLoaderPresenter) for WebLoaderModule

---
+++ app/Presenters/WebLoaderPresenter.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace App\Presenters;
+
+use App\Model\DI;
+use App\Model\Loader\Assets\Module;
+use App\Model\Responses\CSSResponse;
+use App\Model\Responses\JSResponse;
+use App\WebLoader\Exceptions\ParseError;
+use Nette\Application\AbortException;
+use Nette\Application\Responses\TextResponse;
+
+/**
+ * Class WebLoaderPresenter
+ * @package App\Presenters
+ */
+abstract class WebLoaderPresenter extends BasePresenter
+{
+    private Module $module;
+
+    /**
+     * WebLoaderPresenter constructor.
+     * @param Module $module
+     */
+    public function __construct(Module $module)
+    {
+        parent::__construct(DI\GoogleAnalytics::disabled());
+
+        $this->module = $module;
+    }
+
+    /**
+     * @throws AbortException
+     */
+    public function renderCSS() {
+        try {
+            $cssParser = $this->module->getParsedCSS();
+            $parsedCSS = $cssParser->getComputedCode(true);
+            $response = new CSSResponse($parsedCSS);
+            $this->sendResponse($response);
+        } catch (ParseError $e) {
+            $textResponse = new TextResponse($e->getMessage());
+            $this->sendResponse($textResponse);
+        }
+    }
+
+    /**
+     * @throws AbortException
+     */
+    public function renderJS() {
+        $this->sendResponse(new JSResponse($this->module->getParsedJS()));
+    }
+}
