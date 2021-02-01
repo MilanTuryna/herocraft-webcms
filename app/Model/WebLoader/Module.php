8c470e3bb196511bb7ded1180c7f6114f82cbd97
MilanTuryna <miloslavturyna@seznam.cz>
2021-02-01 22:15:22 +0100

    Created abstract class Model\WebLoader\Module(FileMask $cssMask, FileMask $jsMask, ?string $moduleName) for getting minified and compiled assets (css, js) of selected module.

---
+++ app/Model/WebLoader/Module.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace App\Model\Loader\Assets;
+
+use App\WebLoader\Exceptions\ParseError;
+use App\Front\Parsers\Exceptions\SyntaxError;
+use App\Model\Front\Parsers\CSSParser;
+use App\Model\WebLoader\FileMask;
+
+/**
+ * Class Module
+ * @package App\Model\Loader
+ */
+abstract class Module
+{
+    private FileMask $cssMask;
+    private FileMask $jsMask;
+    private string $moduleName;
+
+    /**
+     * Module constructor.
+     * @param FileMask $cssMask
+     * @param FileMask $jsMask
+     * @param string|null $moduleName
+     */
+    public function __construct(FileMask $cssMask, FileMask $jsMask, ?string $moduleName = null)
+    {
+        $this->cssMask = $cssMask;
+        $this->jsMask = $jsMask;
+        $this->moduleName = $moduleName;
+    }
+
+    /**
+     * @return CSSParser
+     * @throws ParseError
+     */
+    public function getParsedCSS(): CSSParser
+    {
+        $rawContent = $this->cssMask->scrapFiles();
+        try {
+            $cssParser = new CSSParser($rawContent, null, false);
+            return $cssParser
+                ->addComment('fsize: ' . strlen($rawContent))
+                ->addComment('web-loader module: ' . $this->moduleName ?? 'undefined name');
+        } catch (SyntaxError $e) {
+            throw new ParseError("An error (SyntaxError) occurred while parsing CSS code in [" . implode(',', $this->cssMask->getMasks()) . "] files.", $e);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getParsedJS(): string {
+        return $this->jsMask->scrapFiles();
+    }
+}
