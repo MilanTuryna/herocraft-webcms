50b44feebc846aeaa3439d7f7b5c1012be4c9ee3
MilanTuryna <miloslavturyna@seznam.cz>
2021-03-07 08:53:12 +0100

    Implemented config/extension.neon as configuration in App\Bootstrap

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -35,7 +35,7 @@ class Bootstrap
 			->register();
 
 		$configurator->addConfig(__DIR__ . '/config/application.neon');
-		$configurator->addConfig(__DIR__ . '/config/extensions.neon.neon');
+		$configurator->addConfig(__DIR__ . '/config/extensions.neon');
         $configurator->addConfig(__DIR__ . '/config/database1.neon');
 		$configurator->addConfig(__DIR__ . '/config/loader.neon');
 		$configurator->addConfig(__DIR__ . '/config/services.neon');
