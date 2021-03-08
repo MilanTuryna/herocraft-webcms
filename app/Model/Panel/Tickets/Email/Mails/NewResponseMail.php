19c55677e99ec3cbf87d38a8a08ce8d848b0f98c
MilanTuryna <miloslavturyna@seznam.cz>
2021-03-08 17:55:46 +0100

    Created Tickets\Email\Mails\NewResponseMail (child of AbstractMail) and created folder Tickets\Email\Latte for saving .latte templates of emails. (Latte\NewResponseMail.latte)

---
+++ app/Model/Panel/Tickets/Email/Mails/NewResponseMail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Model\Panel\Tickets\Email\Mails;
+
+use App\Model\DI\Tickets\EmailSender;
+use App\Model\DI\Tickets\AbstractMail;
+use App\Model\SettingsRepository;
+use Nette\Application\LinkGenerator;
+use Nette\Application\UI\TemplateFactory;
+
+/**
+ * Class NewResponseMail
+ * @package App\Model\Panel\Tickets\Mails
+ */
+class NewResponseMail extends AbstractMail
+{
+    /**
+     * NewResponseMail constructor.
+     * @param EmailSender $emailSender
+     * @param LinkGenerator $linkGenerator
+     * @param TemplateFactory $templateFactory
+     * @param SettingsRepository $settingsRepository
+     */
+    public function __construct(EmailSender $emailSender, LinkGenerator $linkGenerator, TemplateFactory $templateFactory, SettingsRepository $settingsRepository)
+    {
+        parent::__construct($emailSender, $linkGenerator, $templateFactory, $settingsRepository,"NewResponseMail");
+    }
+
+    /**
+     * @param string $responseAuthor
+     * @param string $responseContent
+     * @param int $responseTimestamp
+     * @param string $authorEmail
+     * @param string $ticketId
+     * @return void
+     */
+    public function setEmail(string $responseAuthor, string $responseContent, int $responseTimestamp, string $authorEmail, string $ticketId): void {
+        $this->template->responseAuthor = $responseAuthor;
+        $this->template->responseContent = $responseContent;
+        $this->template->responseTime = $responseTimestamp;
+        $this->message->addTo($authorEmail)->setSubject("Ticket #{$ticketId}: Nová odpověď od {$responseAuthor}");
+    }
+}
